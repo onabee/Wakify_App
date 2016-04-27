2fd7fe0185725390e2319a6135399d0b1bd4c4de
onabee <o.boseroy@gmail.com>
2016-04-26 20:15:48 -0400

    adds js code for trying to get the current time to display. tinkers with CSS

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 console.log("linked!");
+
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
+
+$(document).ready(function(){
+
+	var updateTime = function(){
+		var currentTime = new Date();
+		var hours = currentTime.getHours();
+		var minutes = currentTime.getMinutes();
+		minutes = (minutes < 10 ? "0" : "") + minutes;
+
+		// turns into 12 hour format instead of 24 hours, specifies am or pm
+		var amPm = (hours < 12) ? "am" : "pm";
+		hours = (hours > 12) > hours - 12 : hours;
+		hours = (hours == 0) ? 12 : hours;
+
+		// format how time will look
+		var currentTimeDisplay = hours + ":" + minutes + " " + amPm;
+
+		document.getElementById("clock").firstChild.nodeValue = currentTimeDisplay
+	}
+
+
+});
